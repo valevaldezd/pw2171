17dc37a7449fa9064352f989784e4e96c4967a71
valevaldezd <valevaldezd@gmail.com>
2017-04-03 15:02:44 -0600

    Primer javaScript

---
+++ js/sumas.js
@@ -0,0 +1,15 @@
+function texto(){
+	alert(document.getElementById("texto1").value)
+}
+function suma(a,b){
+			return a+b;
+		}
+		var suma2 = function (a, b){
+			return a+b;
+		}
+function suna3(){
+	var n1 = document.getElementById("num1").value
+	var n2 = document.getElementById("num2").value
+	alert(parseInt (n1)+ parseInt(n2))
+
+}
